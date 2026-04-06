1904761a74dd1017e689aa64b60c0af3789626ee
shah774 <shah-15-@hotmail.com>
2026-04-06 02:47:22 -0500

    Add files via upload

--- home.js
+++
@@ -1,6 +0,0 @@
-import html from "html-literal";
-import * as views from "../views";
-
-export default () => html`
-<h1>Welcome to the Home Page</h1>
-`
--- nav.js
+++ nav.js
@@ -1,7 +1,5 @@
-import html from "html-literal";
-
-export default () => html`
-<nav>
-   <i class="fas fa-bars"></i>
-   <ul class="hidden--mobile nav-links">
-     $(nav)
+export default {
+  [url]: "/home", text: "Home"},
+  [url]: "/about", text: "About"},
+  [url]: "/contact", text: "Contact"}
+}
